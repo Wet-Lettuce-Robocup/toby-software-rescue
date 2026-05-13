909acfaf3f1d45dba7285d9adf75bed51ad7c178
BlueBM <100818726+BlueBM@users.noreply.github.com>
2026-05-13 11:33:41 +1000

    Fixed directory locations, added launch script and tested launch

--- setup.py
+++ setup.py
@@ -10,10 +10,13 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (
+            os.path.join('share', 'ament_index', 'resource_index', 'packages'),
+            [os.path.join('resource', package_name)],
+        ),
+        (os.path.join('share', package_name), ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -28,8 +31,9 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'talker = ml_rescue.ml_rescue:main',
-            'listener = ml_rescue.sub:main',
+            'testpub = ml_rescue.testpub:main',
+            'testsub = ml_rescue.testsub:main',
+            'state_machine = ml_rescue.state_machine:main',
         ],
     },
 )
