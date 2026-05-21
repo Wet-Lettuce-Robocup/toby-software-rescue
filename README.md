b01ac17e5678e9746a5950ba1aba570cebdba9b8
BlueBM <100818726+BlueBM@users.noreply.github.com>
2026-05-22 09:38:09 +1000

    Updated readme with mirroring instructions to the repository that is marked, and created script to take images for ml training

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ NOTE: There is no 'src' directory within this repository, instead it is called '
 
 Using vision based ML running on a Hailo NPU to rescue some balls.
 
+
+old method:
+git remote add external-origin [URL_OF_REPO]
+git fetch external-origin
+git merge external-origin/main --allow-unrelated-histories
+
+better method:
+git clone --mirror https://github.com/user/original-repo.git
+cd original-repo.git
+git push --mirror https://github.com/user/new-repo.git
+
+Repository can be found at https://github.com/Wet-Lettuce-Robocup/toby-software-rescue
---
+++ ml_rescue/image_to_model.py
@@ -0,0 +1,39 @@
+import os
+import time
+
+import cv2
+
+os.makedirs('raw_images', exist_ok=True)
+cap = cv2.VideoCapture(0)
+
+
+class ImageToModel:
+    """
+    A class to capture images using the Raspberry Pi camera and save them for training a YOLOv8 model.
+
+    - Captures images from front camera on robot
+    - Saves images to 'raw_images' directory
+    """
+
+    def __init__(self):
+        self.image_count = 0
+
+    def start_image_stream(self):
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                cv2.imshow('Camera', frame)
+                wait = cv2.waitKey(1)
+                if wait % 256 == 27:  # ESC key to exit
+                    print('Exiting image capture')
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    return False
+
+                elif wait % 256 == 32:  # SPACE key to capture
+                    image_path = f'raw_images/image_{self.image_count}.jpg'
+                    cv2.imwrite(image_path, frame)
+                    print(f'Captured {image_path}')
+                    self.image_count += 1
+            else:
+                print('Failed to capture image')
