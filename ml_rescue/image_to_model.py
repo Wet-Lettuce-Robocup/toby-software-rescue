064b550b44b144259ad73eabb96d66c1caca5ea2
BlueBM <100818726+BlueBM@users.noreply.github.com>
2026-05-22 16:09:46 +1000

    flip image properly in camera feed + set camera settings

--- ml_rescue/image_to_model.py
+++ ml_rescue/image_to_model.py
@@ -6,7 +6,14 @@ from picamera2 import Picamera2
 
 os.makedirs('raw_images', exist_ok=True)
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration())
+picam2.configure(
+    picam2.create_video_configuration(
+        sensor={'output_size': (2304, 1296)},
+        main={'size': (1920, 1080)},
+        controls={'FrameRate': 30},
+    )
+)
+picam2.set_controls({'AfMode': 2})
 picam2.start()
 
 
@@ -24,11 +31,12 @@ class ImageToModel:
 
     def start_image_stream(self):
         while True:
-            time.sleep(1)
+            time.sleep(0.5)
             frame = picam2.capture_array()
             if frame is not None:
                 frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-                cv2.imshow('Camera', frame_bgr)
+                flipped_image = cv2.flip(frame_bgr, 1)
+                cv2.imshow('Camera', flipped_image)
                 wait = cv2.waitKey(1)
                 if wait % 256 == 27:  # ESC key to exit
                     print('Exiting image capture')
@@ -39,7 +47,6 @@ class ImageToModel:
                 elif wait % 256 == 32:  # SPACE key to capture
                     image_path = f'raw_images/image_{self.image_count}.jpg'
 
-                    flipped_image = cv2.flip(frame_bgr, 1)
                     cv2.imwrite(image_path, flipped_image)
 
                     print(f'Captured {image_path}')
