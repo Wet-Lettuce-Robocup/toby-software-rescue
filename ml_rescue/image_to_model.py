79d9e5e7a7854261e32dbd612ed4574b873afc55
BlueBM <100818726+BlueBM@users.noreply.github.com>
2026-05-22 16:47:04 +1000

    fix rotations and apply proper config

--- ml_rescue/image_to_model.py
+++ ml_rescue/image_to_model.py
@@ -2,6 +2,7 @@ import os
 import time
 
 import cv2
+from libcamera import Transform
 from picamera2 import Picamera2
 
 os.makedirs('raw_images', exist_ok=True)
@@ -9,8 +10,9 @@ picam2 = Picamera2()
 picam2.configure(
     picam2.create_video_configuration(
         sensor={'output_size': (2304, 1296)},
-        main={'size': (1920, 1080)},
-        controls={'FrameRate': 30},
+        main={'size': (960, 540)},
+        controls={'FrameRate': 10},
+        transform=Transform(hflip=1, vflip=1),  # 180 degree rotation
     )
 )
 picam2.set_controls({'AfMode': 2})
@@ -31,12 +33,11 @@ class ImageToModel:
 
     def start_image_stream(self):
         while True:
-            time.sleep(0.5)
+            time.sleep(0.1)
             frame = picam2.capture_array()
             if frame is not None:
                 frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-                flipped_image = cv2.flip(frame_bgr, 1)
-                cv2.imshow('Camera', flipped_image)
+                cv2.imshow('Camera', frame_bgr)
                 wait = cv2.waitKey(1)
                 if wait % 256 == 27:  # ESC key to exit
                     print('Exiting image capture')
@@ -47,7 +48,7 @@ class ImageToModel:
                 elif wait % 256 == 32:  # SPACE key to capture
                     image_path = f'raw_images/image_{self.image_count}.jpg'
 
-                    cv2.imwrite(image_path, flipped_image)
+                    cv2.imwrite(image_path, frame_bgr)
 
                     print(f'Captured {image_path}')
                     self.image_count += 1
