6291fae631dee31e959fccf18b707e1dc53fb906
BlueBM <100818726+BlueBM@users.noreply.github.com>
2026-06-17 20:29:56 +1000

    Update ml_vision_node.py

--- src/ml_rescue/ml_rescue/ml_vision_node.py
+++ src/ml_rescue/ml_rescue/ml_vision_node.py
@@ -8,8 +8,8 @@ from cv_bridge import CvBridge
 from hailo_platform import VDevice
 import numpy as np
 import rclpy
-from rclpy.node import Node
 from rescue_msgs.srv import EnableInference
+from rclpy.node import Node
 from sensor_msgs.msg import Image
 from vision_msgs.msg import Detection2D, Detection2DArray
 
