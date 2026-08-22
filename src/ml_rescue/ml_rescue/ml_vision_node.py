a3b25a42bdd54e7e5165d5f6fd46e455b1080544
BlueBM <100818726+BlueBM@users.noreply.github.com>
2026-08-22 21:37:53 +1000

    i forgot how good dictionaries are. anyways i completely rewrote inference to ditch the hailo npu because i want rescue to work at state

--- src/ml_rescue/ml_rescue/ml_vision_node.py
+++ src/ml_rescue/ml_rescue/ml_vision_node.py
@@ -8,7 +8,6 @@ import queue
 from ament_index_python.packages import get_package_share_directory
 import cv2
 from cv_bridge import CvBridge
-from hailo_platform import VDevice
 import numpy as np
 import rclpy
 from rclpy.node import Node
@@ -19,6 +18,7 @@ from vision_msgs.msg import (
     Detection2DArray,
     ObjectHypothesisWithPose,
 )
+from ultralytics import YOLO
 
 
 class VisionNode(Node):
@@ -48,13 +48,14 @@ class VisionNode(Node):
             EnableInference, 'enable_inference', self.rescue_active_callback
         )
 
+        self.fps = 20
+        self.timer = self.create_timer(1 / self.fps, self.run_inference)
+
         # Camera and inference setup
 
         self.isActive = False
 
         self.bridge = CvBridge()
-
-        self.fps = 10
 
         self.counts = 0
 
@@ -62,28 +63,15 @@ class VisionNode(Node):
         self.latest_cropped_image = None
         self.latest_image_header = None
 
-        self.hailo = 'state_yolov8n'  # Model filename
-        self.hef_path = os.path.join(
-            get_package_share_directory('ml_rescue'), 'modelhef', f'{self.hailo}.hef'
+        self.current_data = {}
+
+        self.model_name = 'state.pt'  # Model filename
+        self.model_path = os.path.join(
+            get_package_share_directory('ml_rescue'), 'modelpt', f'{self.model_name}'
         )
+        self.model = YOLO(self.model_path)
         self.imgsz = 640
-        self.conf_threshold = 0.8
-        self.model_classes = ['silver', 'black', 'green', 'red']
-
-        self.results_queue = queue.Queue(maxsize=2)
-
-        try:
-            self.target = VDevice()
-            self.infer_model = self.target.create_infer_model(self.hef_path)
-            self.input_name = self.infer_model.input_names[0]
-            self.output_name = self.infer_model.output_names[0]
-            self.output_shape = self.infer_model.output(self.output_name).shape
-            self.configured_model = self.infer_model.configure()
-
-        except Exception as e:
-            self.get_logger().error(f'Error loading hailort: {e}')
-            self.target = None
-            raise
+        self.conf = 0.6
 
         self.out = None
 
@@ -91,8 +79,8 @@ class VisionNode(Node):
         self.dw = 1536
         self.dh = 864
 
-        self.cdw = 1384
-        self.cdh = 559
+        self.start_x = 0
+        self.start_y = 0
 
         # Video writer setup for debugging
         pipeline = (
@@ -107,13 +95,18 @@ class VisionNode(Node):
 
     def rescue_active_callback(self, request, response):
         """Enables/disables inference functionality."""
-        self.isActive = request.enabled
-        response.message = (
-            'Inference enabled successfully' if request.enabled else 'Inference disabled'
-        )
+        if request.enabled:
+            self.isActive = True
+            response.message = 'Inference enabled succesfully'
+            self.timer.reset()
+        elif not request.enabled:
+            self.isActive = False
+            response.message = 'Inference disabled'
+            self.timer.cancel()
+        else:
+            self.get_logger().error('Inference toggle failed')
 
         self.get_logger().info(response.message)
-
         return response
 
     def image_callback(self, msg):
@@ -138,172 +131,125 @@ class VisionNode(Node):
         start_x = side_margin
         end_x = width - side_margin
 
+        self.start_x = start_x
+        self.start_y = start_y
+
         self.latest_cropped_image = frame[start_y:end_y, start_x:end_x]
+
+    def crop_box_to_frame(self, x1, y1, x2, y2):
+        return (
+            int(x1 + self.start_x),
+            int(y1 + self.start_y),
+            int(x2 + self.start_x),
+            int(y2 + self.start_y),
+        )
+
+    def run_inference(self):
+        if self.latest_cropped_image is None:
+            return
+
+        # Creates a copy of latest image so that latest_image can asynchronously update
+        image_header = self.latest_image_header
+        cropped_frame = self.latest_cropped_image.copy()
+
+        results = self.model.predict(cropped_frame, conf=self.conf, stream=True, imgsz=self.imgsz)
+
+        all_detections = {}
+
+        counts = {'silver': 0, 'black': 0, 'green': 0, 'red': 0}
+
+        for i in results:
+            if self.debug:
+                annotated_frame = i.plot()
+                cv2.imshow('a', annotated_frame)
+                cv2.waitKey(1)
+                # self.out.write(annotated_frame)
+
+            for x1, y1, x2, y2, conf, cls in i.boxes.data.tolist():
+                self.get_logger().info(str(i.boxes))
+
+                x1, y1, x2, y2 = self.crop_box_to_frame(x1, y1, x2, y2)
+
+                class_name = self.model.names[int(cls)]
+                self.get_logger().info(f'{cls} detected')
+
+                data = {'cls': class_name, 'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2, 'conf': conf}
+
+                if class_name in counts:
+                    counts[class_name] += 1
+                all_detections.update({len(all_detections): data})
+
+        all_data = {'header': image_header, 'detections': all_detections, 'counts': counts}
+
+        self.get_logger().info(str(all_data))
+        self.current_data = all_data
 
     def inference_callback(self, request, response):
         """Runs inference on latest camera frame and returns detections."""
-        # self.get_logger().info('Inference has been called.')
 
         if not self.isActive:
             self.get_logger().warn('Inference called but is not active.')
             response.success = False
             return response
 
-        if self.latest_image is None:
+        if self.latest_cropped_image is None:
             self.get_logger().warn('No image received, is the front camera working?')
             response.success = False
             return response
 
         # Sets a filter for either balls or evacuation points, depending on what is requested
-        if request.message == 'ball':
-            mode = 1
-            # self.get_logger().info('A ball has been requested')
-        elif request.message == 'evacpoint':
-            mode = 2
+        if request.message == 'ball' or request.message == 'evacpoint':
+            pass
         else:
             self.get_logger().info(f'Invalid inference request {request.message}')
             response.success = False
             return response
 
-        # start_time = time.time()
-
-        # Creates a copy of latest image so that latest_image can asynchronously update
-        image_header = self.latest_image_header
-        raw_frame = self.latest_cropped_image.copy()
         detection_msg = None
 
-        # self.out.write(raw_frame)
+        all_data = self.current_data
 
-        # Convert frame to an array for processing
-        resized_frame = cv2.resize(raw_frame, (self.imgsz, self.imgsz))
-        input_data = np.ascontiguousarray(resized_frame)
+        # Don't parse frames with no detections
+        if not all_data['detections']:
+            response.success = False
+            return response
 
-        # Create Hailo bindings
-        bindings = self.configured_model.create_bindings()
-        bindings.input(self.input_name).set_buffer(input_data)
+        image_header = all_data['header']
 
-        output_buffer = np.zeros(self.output_shape, dtype=np.float32)
-        bindings.output(self.output_name).set_buffer(output_buffer)
+        # Format data as a Detection2DArray to send to rescue node
+        detection_msg = Detection2DArray()
+        detection_msg.header.stamp = image_header.stamp
+        detection_msg.header.frame_id = image_header.frame_id
 
-        bound_callback = partial(
-            self._inference_callback,
-            output_buffer=output_buffer,
-            display_frame=raw_frame,
-        )
+        # Filter results based on request
+        if request.message == 'ball' and (
+            all_data['counts']['silver'] > 0 or all_data['counts']['black'] > 0
+        ):
+            filter = 'ball'
+        elif request.message == 'evacpoint' and (
+            all_data['counts']['green'] > 0 or all_data['counts']['red'] > 0
+        ):
+            filter = 'point'
+        else:
+            # Don't parse frames with no valid detections
+            response.success = False
+            return response
 
-        # self.configured_model.wait_for_async_ready(timeout_ms=1000)
+        for i in all_data['detections'].values():
+            if (filter == 'ball' and i['cls'] == 'silver') or (
+                filter == 'point' and i['cls'] == 'green'
+            ):
+                detection = self.parse_datapoint(i, detection_msg)
+                detection_msg.detections.append(detection)  # To be sent to ml_rescue_node
 
-        # if self.inferenceBusy:
-        #     return
+        # Separate for loop to ensure that silver and green have priority
+        for i in all_data['detections'].values():
+            if (filter == 'ball' and i['cls'] == 'black') or (
+                filter == 'point' and i['cls'] == 'red'
+            ):
+                detection = self.parse_datapoint(i, detection_msg)
+                detection_msg.detections.append(detection)  # To be sent to ml_rescue_node
 
-        # self.inferenceBusy = True
-
-        # Run inference and wait for return
-        job = self.configured_model.run_async([bindings], bound_callback)
-        job.wait(1000)
-
-        try:
-            vis_frame, latest_objects = self.results_queue.get_nowait()
-
-            # self.get_logger().info(f'balls: {latest_balls}')
-
-            # Format data as a Detection2DArray to send to rescue node
-            detection_msg = Detection2DArray()
-            detection_msg.header.stamp = image_header.stamp
-            detection_msg.header.frame_id = image_header.frame_id
-
-            for object in latest_objects:
-                y1, x1, y2, x2 = object['box']
-                score = object['score']
-                material = object['class_name']
-
-                self.get_logger().info(f'{material} detected')
-
-                # Scale values back to original frame size
-                px1 = int(x1 * self.dw)
-                py1 = int(y1 * self.dh)
-                px2 = int(x2 * self.dw)
-                py2 = int(y2 * self.dh)
-
-                # Clamp boxes inside your image frames
-                px1, px2 = max(0, min(self.dw, px1)), max(0, min(self.dw, px2))
-                py1, py2 = max(0, min(self.dh, py1)), max(0, min(self.dh, py2))
-
-                pxc = (px1 + px2) / 2
-                pyc = (py1 + py2) / 2
-
-                # # Backup code if colour detection breaks
-                # roi = vis_frame[py1:py2, px1:px2]
-                # gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-
-                # mean = gray.mean()
-
-                # if mean > 100:
-                #     material = 'silver'
-                #     # self.get_logger().info(f'Mean is {mean} detected as silver')
-                # else:
-                #     material = 'black'
-                #     # self.get_logger().info(f'Mean is {mean} detected as black')
-
-                # Filter results based on request
-                if (
-                    mode == 1
-                    and material in ['silver', 'black']
-                    or mode == 2
-                    and material in ['green', 'red']
-                    or material == 'obstacle'
-                ):
-                    detection = Detection2D()
-                    detection.header = detection_msg.header
-
-                    detection.bbox.center.position.x = pxc
-                    detection.bbox.center.position.y = pyc
-                    detection.bbox.size_x = float(px2 - px1)
-                    detection.bbox.size_y = float(py2 - py1)
-
-                    hypothesis = ObjectHypothesisWithPose()
-                    hypothesis.hypothesis.class_id = material
-                    hypothesis.hypothesis.score = float(score)
-
-                    detection.results.append(hypothesis)
-                    detection_msg.detections.append(detection)  # To be sent to ml_rescue_node
-
-                if self.debug:
-                    # Output an annotated frame with objects outlined
-                    pxc, pyc, px1, py1, px2, py2 = map(int, (pxc, pyc, px1, py1, px2, py2))
-
-                    # Red outline for bounding box and dot for centre of object
-                    cv2.rectangle(vis_frame, (px1, py1), (px2, py2), (0, 0, 255), 2)
-                    cv2.circle(vis_frame, (pxc, pyc), 2, (0, 0, 255), -1)
-                    # self.get_logger().info(
-                    #     f'Object detected at ({pxc},{pyc}) with confidence {score}'
-                    # )
-
-                    label = f'{material} {score:.2f}'  # Label with confidence score
-                    cv2.putText(
-                        vis_frame,
-                        label,
-                        (px1, max(20, py1 - 5)),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5,
-                        (0, 0, 255),
-                        2,
-                    )
-
-            # inf_time_elapsed = time.time() - start_time
-            # if self.debug:
-            #     self.get_logger().info(f'--- Just for inference: {inf_time_elapsed:.2f} ---')
-
-            # Render frame to video
-            if self.debug:
-                cv2.imshow('a', vis_frame)
-                cv2.waitKey(1)
-                self.out.write(vis_frame)
-
-        except queue.Empty:
-            pass
-
-        # self.get_logger().info('- - - Sending detections - - -')
         if detection_msg is not None and detection_msg.detections:
             response.success = True
             response.detections = detection_msg
@@ -314,68 +260,22 @@ class VisionNode(Node):
 
         return response
 
-        # time_elapsed = time.time() - start_time
-        # if self.debug:
-        #     self.get_logger().info(f'--- Total time elapsed: {time_elapsed:.2f}s ---')
-        #     self.get_logger().info(
-        #         f'Time for contouring: {(time_elapsed - inf_time_elapsed):.2f}s'
-        #     )
+    def parse_datapoint(self, datapoint, detection_msg) -> Detection2D:
+        detection = Detection2D()
+        detection.header = detection_msg.header
 
-    def _inference_callback(self, completion_info, output_buffer=None, display_frame=None):
-        """Post-processing to clean up inference data."""
-        flat_buffer = output_buffer.flatten()
-        detections = []
+        detection.bbox.center.position.x = (datapoint['x2'] + datapoint['x1']) / 2
+        detection.bbox.center.position.y = (datapoint['y2'] + datapoint['y1']) / 2
+        detection.bbox.size_x = float(datapoint['x2'] - datapoint['x1'])
+        detection.bbox.size_y = float(datapoint['y2'] - datapoint['y1'])
 
-        idx = 0
-        vals = 5  # Each object has 5 points of data
-        max_dets = 100  # Maximum of 100 detections are returned, should lower in future
+        hypothesis = ObjectHypothesisWithPose()
+        hypothesis.hypothesis.class_id = datapoint['cls']
+        hypothesis.hypothesis.score = float(datapoint['conf'])
 
-        # self.get_logger().info(f'output_buffer.shape = {output_buffer.shape}')
-        # self.get_logger().info(f'flat_buffer[:80] = {flat_buffer[:80]}')
+        detection.results.append(hypothesis)
 
-        for class_id, class_name in enumerate(self.model_classes):
-            if idx >= len(flat_buffer):
-                self.get_logger().warn(
-                    f'Ran out of buffer before reading count for class {class_name} at idx={idx}'
-                )
-                break
-            num_detections = int(flat_buffer[idx])
-            idx += 1
-
-            # self.get_logger().info(f'class {class_name}: {num_detections} detections')
-
-            num_detections = max(0, min(num_detections, max_dets))  # just in case
-
-            # Iterate through detections
-            for det_i in range(num_detections):
-                if idx + vals > len(flat_buffer):
-                    self.get_logger().warn(
-                        f'Ran out of buffer while reading detection {det_i} for class {class_name}'
-                    )
-                    break
-
-                y1 = flat_buffer[idx]
-                x1 = flat_buffer[idx + 1]
-                y2 = flat_buffer[idx + 2]
-                x2 = flat_buffer[idx + 3]
-                score = flat_buffer[idx + 4]
-
-                idx += vals
-
-                # Only return data points with a high enough confidence
-                if score >= self.conf_threshold:
-                    detections.append({
-                        'box': [y1, x1, y2, x2],
-                        'score': score,
-                        'class_id': class_id,
-                        'class_name': class_name,
-                    })
-
-        # Push both the frame and its matching detections to the main thread
-        if not self.results_queue.full():
-            self.results_queue.put_nowait((display_frame, detections))
-
-        # self.inferenceBusy = False
+        return detection
 
 
 def main(args=None):
@@ -389,8 +289,6 @@ def main(args=None):
         if vision_node is not None:
             if vision_node.out is not None:
                 vision_node.out.release()
-            if vision_node.target is not None:
-                vision_node.target.release()
             vision_node.destroy_node()
         rclpy.shutdown()
 
